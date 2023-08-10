347d6227125b0f352f5bfa9a87ef1353315196f0
naiiiden <na4ko359@gmail.com>
2023-08-10 18:47:23 +0300

    test import header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  hi
+  <Header/>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <h1>podia</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
