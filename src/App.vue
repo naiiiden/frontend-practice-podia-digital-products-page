7fa314c08224731d5b3e1601b3c59d159a1d46a2
naiiiden <na4ko359@gmail.com>
2023-08-19 17:22:17 +0300

    Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,14 @@
 <script setup>
 import Header from './components/header/Header.vue';
 import { openDropdown } from './components/header/openDropdown';
+import Hero from './components/hero/Hero.vue';
 </script>
 
 <template>
   <Header/>
   <div @click="openDropdown = !openDropdown">
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
-    <p>hi</p>
+    <main>
+      <Hero/>
+    </main>
   </div>
 </template>
---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    headingPreText: String,
+    heading1: String,
+    heading2: String,
+    paragraph: String,
+    link: String,
+    img: String,
+})
+</script>
+
+<template>
+    <div class="hero-container">
+        <div class="hero-text">
+            <span>{{ headingPreText }}</span>
+            <h1 v-if="heading1">{{ heading1 }}</h1>
+            <h2 v-if="heading2">{{ heading2 }}</h2>
+            <p>{{ paragraph }}</p>
+            <a href="">{{ link }}</a>
+        </div>
+        <img src="{{ img }}" alt="">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
