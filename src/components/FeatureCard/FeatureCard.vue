8ae59ef22569dcdf2520df13eb3d5037a41171de
naiiiden <na4ko359@gmail.com>
2023-08-20 23:47:33 +0300

    featurecard component

---
+++ src/components/FeatureCard/FeatureCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    img: String,
+    h3: String,
+    paragraph: String,
+})
+</script>
+
+<template>
+    <div class="feature-card">
+        <img :src=img alt=""/>
+        <h3>{{ h3 }}</h3>
+        <p>{{ paragraph }}</p>
+    </div>
+</template>
