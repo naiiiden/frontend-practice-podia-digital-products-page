309fd14cb11b47ebfcc423037e5efc98664d974a
naiiiden <na4ko359@gmail.com>
2023-08-24 14:01:44 +0300

    footer nav skeleton

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,10 +1,33 @@
 <script setup>
 
 </script>
+
 <template>
     <footer>
         <div class="footer-content">
-            hi
+            <nav>
+                <ul>
+                    <li>
+                        <button class="footer-mobile-btn">
+                            Platform
+                            <svg width="10" viewBox="0 0 10 6" fill="none">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l4 4 4-4"></path>
+                            </svg>
+                        </button>
+                        <h3 class="footer-desktop-section-link">
+                           Platform 
+                        </h3>
+                        <ul>
+                            <li><a href="">Pricing</a></li>
+                            <li><a href="">Sign up free</a></li>
+                            <li><a href="">Community</a></li>
+                            <li><a href="">Website builder</a></li>
+                            <li><a href="">Digital products</a></li>
+                            <li><a href="">Email marketing</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </footer>
 </template>
