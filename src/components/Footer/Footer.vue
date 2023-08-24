1ab45d142cbe2c0faa7ad58e728332eab0bab91e
naiiiden <na4ko359@gmail.com>
2023-08-24 14:26:11 +0300

    add footer links into object so i can list over them 4 practice

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,195 @@
 <script setup>
 import FooterNavLi from './FooterNavLi.vue';
 
+const footerNavList = ref([
+    {
+        liName: 'Platform',
+        links: [
+            {
+                href: '',
+                text: 'Pricing',
+            },
+            {
+                href: '',
+                text: 'Sign up free',
+            },
+            {
+                href: '',
+                text: 'Community',
+            },
+            {
+                href: '',
+                text: 'Website builder',
+            },
+            {
+                href: '',
+                text: 'Digital products',
+            },
+            {
+                href: '',
+                text: 'Email marketing',
+            },
+        ]
+    }, 
+    {
+        liName: 'Support',
+        links: [
+            {
+                href: '',
+                text: 'Contact us',
+            },
+            {
+                href: '',
+                text: 'Help center',
+            },
+            {
+                href: '',
+                text: 'Hire a Podia pro',
+            },
+            {
+                href: '',
+                text: 'Creator community',
+            },
+            {
+                href: '',
+                text: 'Podia 101',
+            },
+        ]
+    },
+    {
+        liName: 'Features',
+        links: [
+            {
+                href: '',
+                text: 'Online courses',
+            },
+            {
+                href: '',
+                text: 'Downloads',
+            },
+            {
+                href: '',
+                text: 'Webinars',
+            },
+            {
+                href: '',
+                text: 'Coaching',
+            },
+            {
+                href: '',
+                text: 'All features',
+            },
+        ]
+    },
+    {
+        liName: 'Compare',
+        links: [
+            {
+                href: '',
+                text: 'Kajabi',
+            },
+            {
+                href: '',
+                text: 'Teachable',
+            },
+            {
+                href: '',
+                text: 'Thinkific',
+            },
+            {
+                href: '',
+                text: 'Patreon',
+            },
+            {
+                href: '',
+                text: 'Gumroad',
+            },
+            {
+                href: '',
+                text: 'Podia alternatives',
+            },
+        ]
+    }, 
+    {
+        liName: 'Resources',
+        links: [
+            {
+                href: '',
+                text: 'Examples',
+            },
+            {
+                href: '',
+                text: 'Demo',
+            },
+            {
+                href: '',
+                text: 'Creator stories',
+            },
+            {
+                href: '',
+                text: 'Blog',
+            },
+            {
+                href: '',
+                text: 'Free tools',
+            },
+            {
+                href: '',
+                text: 'All resources',
+            },
+        ]
+    },
+    {
+        liName: 'Podia',
+        links: [
+            {
+                href: '',
+                text: 'Jobs',
+            },
+            {
+                href: '',
+                text: 'About',
+            },
+            {
+                href: '',
+                text: 'Branding',
+            },
+            {
+                href: '',
+                text: 'Reviews',
+            },
+            {
+                href: '',
+                text: 'Refer-a-creator',
+            },
+            {
+                href: '',
+                text: 'Affiliates',
+            },
+        ]
+    },
+    {
+        liName: 'Social',
+        links: [
+            {
+                href: '',
+                text: 'Twitter',
+            },
+            {
+                href: '',
+                text: 'Facebook',
+            },
+            {
+                href: '',
+                text: 'Instagram',
+            },
+            {
+                href: '',
+                text: 'Youtube',
+            },
+        ]
+    }
+])
 </script>
 
 <template>
@@ -8,52 +197,6 @@ import FooterNavLi from './FooterNavLi.vue';
         <div class="footer-content">
             <nav>
                 <ul>
-                    <FooterNavLi liName="Platform">
-                        <li><a href="">Pricing</a></li>
-                        <li><a href="">Sign up free</a></li>
-                        <li><a href="">Community</a></li>
-                        <li><a href="">Website builder</a></li>
-                        <li><a href="">Digital products</a></li>
-                        <li><a href="">Email marketing</a></li>
-                    </FooterNavLi>
-                    <FooterNavLi liName="Support">
-                        <li><a href="">Contact us</a></li>
-                        <li><a href="">Help center</a></li>
-                        <li><a href="">Hire a Podia pro</a></li>
-                        <li><a href="">Creator community</a></li>
-                        <li><a href="">Podia 101</a></li>
-                    </FooterNavLi>
-                    <FooterNavLi liName="Features">
-                        <li><a href="">Online courses</a></li>
-                        <li><a href="">Downloads</a></li>
-                        <li><a href="">Webinars</a></li>
-                        <li><a href="">Coaching</a></li>
-                        <li><a href="">All features</a></li>
-                    </FooterNavLi>
-                    <FooterNavLi liName="Compare">
-                        <li><a href="">Kajabi</a></li>
-                        <li><a href="">Teachable</a></li>
-                        <li><a href="">Thinkific</a></li>
-                        <li><a href="">Patreon</a></li>
-                        <li><a href="">Gumroad</a></li>
-                        <li><a href="">Podia alternatives</a></li>
-                    </FooterNavLi>
-                    <FooterNavLi liName="Resources">
-                        <li><a href="">Examples</a></li>
-                        <li><a href="">Demo</a></li>
-                        <li><a href="">Creator stories</a></li>
-                        <li><a href="">Blog</a></li>
-                        <li><a href="">Free tools</a></li>
-                        <li><a href="">All resources</a></li>
-                    </FooterNavLi>
-                    <FooterNavLi liName="Podia">
-                        <li><a href="">Jobs</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Branding</a></li>
-                        <li><a href="">Reviews</a></li>
-                        <li><a href="">Refer-a-creator</a></li>
-                        <li><a href="">Affiliates</a></li>
-                    </FooterNavLi>
                     <FooterNavLi liName="Social">
                         <li><a href="">Twitter</a></li>
                         <li><a href="">Facebook</a></li>
