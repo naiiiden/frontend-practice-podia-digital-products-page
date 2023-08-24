9dda928e5db8e1d641317920a7cd952d1d80b1fc
naiiiden <na4ko359@gmail.com>
2023-08-24 14:15:51 +0300

    add rest of links

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -8,29 +8,57 @@ import FooterNavLi from './FooterNavLi.vue';
         <div class="footer-content">
             <nav>
                 <ul>
-                    <li>
-                        <button class="footer-mobile-btn">
-                            Platform
-                            <svg width="10" viewBox="0 0 10 6" fill="none">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l4 4 4-4"></path>
-                            </svg>
-                        </button>
-                        <h3 class="footer-desktop-section-link">
-                           Platform 
-                        </h3>
-                        <ul>
-                            <li><a href="">Pricing</a></li>
-                            <li><a href="">Sign up free</a></li>
-                            <li><a href="">Community</a></li>
-                            <li><a href="">Website builder</a></li>
-                            <li><a href="">Digital products</a></li>
-                            <li><a href="">Email marketing</a></li>
-                        </ul>
-                    </li>
-                    <FooterNavLi liName="haha">
-                        <li><a href="">1</a></li>
-                        <li><a href="">2</a></li>
-                        <li><a href="">3</a></li>
+                    <FooterNavLi liName="Platform">
+                        <li><a href="">Pricing</a></li>
+                        <li><a href="">Sign up free</a></li>
+                        <li><a href="">Community</a></li>
+                        <li><a href="">Website builder</a></li>
+                        <li><a href="">Digital products</a></li>
+                        <li><a href="">Email marketing</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Support">
+                        <li><a href="">Contact us</a></li>
+                        <li><a href="">Help center</a></li>
+                        <li><a href="">Hire a Podia pro</a></li>
+                        <li><a href="">Creator community</a></li>
+                        <li><a href="">Podia 101</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Features">
+                        <li><a href="">Online courses</a></li>
+                        <li><a href="">Downloads</a></li>
+                        <li><a href="">Webinars</a></li>
+                        <li><a href="">Coaching</a></li>
+                        <li><a href="">All features</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Compare">
+                        <li><a href="">Kajabi</a></li>
+                        <li><a href="">Teachable</a></li>
+                        <li><a href="">Thinkific</a></li>
+                        <li><a href="">Patreon</a></li>
+                        <li><a href="">Gumroad</a></li>
+                        <li><a href="">Podia alternatives</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Resources">
+                        <li><a href="">Examples</a></li>
+                        <li><a href="">Demo</a></li>
+                        <li><a href="">Creator stories</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Free tools</a></li>
+                        <li><a href="">All resources</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Podia">
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Branding</a></li>
+                        <li><a href="">Reviews</a></li>
+                        <li><a href="">Refer-a-creator</a></li>
+                        <li><a href="">Affiliates</a></li>
+                    </FooterNavLi>
+                    <FooterNavLi liName="Social">
+                        <li><a href="">Twitter</a></li>
+                        <li><a href="">Facebook</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Youtube</a></li>
                     </FooterNavLi>
                 </ul>
             </nav>
